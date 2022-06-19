8c51f2136b9a4a06825bf9a2e56f2b31719404ba
NeverlandMJ <khasanovasumbula@gmail.com>
2022-06-19 23:11:31 +0500

    review

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,9 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	//Place your code here
+	n := len(input)
+	for i := n-1; i>=0; i--{
+		result = append(result, input[i])
+	}
 	return
 }
